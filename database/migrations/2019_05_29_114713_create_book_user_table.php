c71afea596971884fac3bd9132546cc0afe079a6
Tresmollo <Tresmollo@users.noreply.github.com>
2019-05-29 14:39:08 +0200

    Migrations and seeders

---
+++ database/migrations/2019_05_29_114713_create_book_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('book_id');
+            $table->bigIncrements('user_id');
+            $table->date('date_out');
+            $table->date('date_in');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_user');
+    }
+}
